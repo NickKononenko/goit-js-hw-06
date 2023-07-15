eb4052dc45b03cc2b6861a38861c7bd9deb80ef7
NickKononenko <kononenko0812@gmail.com>
2023-07-15 20:34:58 +0300

    final commit

--- homework 1/js/task-01.js
+++
@@ -1,9 +0,0 @@
-const list = document.querySelector("#categories");
-
-const listItems = [...list.children];
-console.log(`Number of categories: ${listItems.length}`);
-
-listItems.forEach((item) => {
-  console.log(`Category: ${item.firstElementChild.textContent}`);
-  console.log(`Elements: ${item.lastElementChild.children.length}`);
-});
